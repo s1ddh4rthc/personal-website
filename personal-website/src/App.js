82d2c615459912e833a6af7495ee958b432f2ef6
Siddharth Cherukupalli <cherukusid@gmail.com>
2024-01-10 23:29:24 -0800

    Got main page to work for now, static content

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,46 +1,37 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-
 import Header from './components/Header';
 import Education from './components/Education';
 import WorkExperience from './components/WorkExperience';
-// import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './styles.css'; // Import the styles.css file
-// import Navbar from './components/Navbar';
+import './styles.css';
 
 const App = () => {
-  const workExperiences = [
-    {
-      companyLogo: 'path/to/company1-logo.png',
-      role: 'Software Engineer Intern',
-      duration: 'Dec 2023 – Present',
-      details: [
-        'Worked on the GuardX project to simulate cyber attacks.',
-        'Streamlined workflows by implementing CI/CD pipelines.',
-        'Leveraged Elasticsearch, Logstash, Kibana stack for log analysis.',
-      ],
-    },
-    // Add more experiences as needed
-  ];
+  // const workExperiences = [
+  //   {
+  //     companyLogo: 'path/to/company1-logo.png',
+  //     role: 'Software Engineer Intern',
+  //     duration: 'Dec 2023 – Present',
+  //     details: [
+  //       'Worked on the GuardX project to simulate cyber attacks.',
+  //       'Streamlined workflows by implementing CI/CD pipelines.',
+  //       'Leveraged Elasticsearch, Logstash, Kibana stack for log analysis.',
+  //     ],
+  //   },
+  //   // Add more experiences as needed
+  // ];
 
   return (
-    <Router>
-      <div>
-        <Routes> {/* Use 'Routes' instead of 'Switch' */}
-          <Route path="/about" element={<Header />} />
-          <Route path="/experience" element={<WorkExperience experiences={workExperiences} />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Education />
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+    <div>
+      <Header />        
+      <WorkExperience />        
+      <Contact />
+      <Education />
+      <Footer />
+    </div>
+    );
+  };
 
 export default App;
 
--- personal-website/src/components/Navbar.js
+++ personal-website/src/components/Navbar.js
@@ -8,13 +8,13 @@ const Navbar = () => {
     <nav>
       <ul>
         <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
           <Link to="/about">About</Link>
         </li>
         <li>
           <Link to="/experience">Experience</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
         </li>
         <li>
           <Link to="/contact">Contact</Link>
